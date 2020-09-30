6d730aba0d8823cb761bb3c5df66ba3027f913c8
AndresArdila89 <andresardila@me.com>
2020-09-30 15:29:18 -0400

    first commit

---
+++ Hi-Tech_Order_Management/Hi-Tech_Order_Management/DAL/UtilityDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Hi_Tech_Order_Management.DAL
+{
+    public static class UtilityDB
+    {
+        public static SqlConnection ConnectDB()
+        {
+            SqlConnection connDB = new SqlConnection();
+            connDB.ConnectionString = ConfigurationManager.ConnectionStrings["HiTechDB_Connection"].ConnectionString;
+            connDB.Open();
+
+            return connDB;
+        }
+    }
+}
